48b22a05b86ee89d73b0e0c21aa158b6993ed464
FangBin <52097203+exquisite-Ceramic@users.noreply.github.com>
2026-08-29 17:54:33 +0800

    feat(step28): add approval scope package API

---
+++ platform/approval_scope/src/design_approval_scope/__init__.py
@@ -0,0 +1,48 @@
+"""Public API for Step28 approval effect scope."""
+
+from .contracts import (
+    ApprovalScopeBoundary,
+    ApprovalScopeDefinition,
+    ApprovalScopeError,
+    ApprovalScopePlanRequest,
+    CanonicalAspect,
+    CanonicalEffectEvidence,
+    CreationRule,
+    DeletionRule,
+    DirectEntityEffect,
+    EntityPredicate,
+    EntitySelector,
+    ExecutionSliceScopeRule,
+    ExistingEntityRule,
+    PredicateField,
+    PredicateOperator,
+    PredicateTerm,
+    ScopeEffectRecipe,
+)
+from .hashing import bind_changeset, compute_scope_body_hash
+from .planner import ApprovalScopePlanner, direct_existing_rule_id, recipe_existing_rule_id
+
+__all__ = [
+    "ApprovalScopeBoundary",
+    "ApprovalScopeDefinition",
+    "ApprovalScopeError",
+    "ApprovalScopePlanRequest",
+    "ApprovalScopePlanner",
+    "CanonicalAspect",
+    "CanonicalEffectEvidence",
+    "CreationRule",
+    "DeletionRule",
+    "DirectEntityEffect",
+    "EntityPredicate",
+    "EntitySelector",
+    "ExecutionSliceScopeRule",
+    "ExistingEntityRule",
+    "PredicateField",
+    "PredicateOperator",
+    "PredicateTerm",
+    "ScopeEffectRecipe",
+    "bind_changeset",
+    "compute_scope_body_hash",
+    "direct_existing_rule_id",
+    "recipe_existing_rule_id",
+]
