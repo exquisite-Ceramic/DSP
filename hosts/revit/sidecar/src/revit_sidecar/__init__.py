e4ea3ea5fbd982f3adae46349bc1120512c1781c
FangBin <52097203+exquisite-Ceramic@users.noreply.github.com>
2026-09-25 15:47:33 +0800

    feat: expose Revit context and snapshot read ports

--- hosts/revit/sidecar/src/revit_sidecar/__init__.py
+++ hosts/revit/sidecar/src/revit_sidecar/__init__.py
@@ -1,6 +1,23 @@
 """Public Revit sidecar API."""
 
+from .context import (
+    RevitContextObservation,
+    RevitContextReadPort,
+    RevitSelectedElement,
+)
 from .model_adapter import RevitHostAdapter
 from .named_pipe import NamedPipeTransport
+from .snapshot_read import (
+    RevitWallThicknessSnapshotEvidence,
+    RevitWallThicknessSnapshotReadPort,
+)
 
-__all__ = ["NamedPipeTransport", "RevitHostAdapter"]
+__all__ = [
+    "NamedPipeTransport",
+    "RevitContextObservation",
+    "RevitContextReadPort",
+    "RevitHostAdapter",
+    "RevitSelectedElement",
+    "RevitWallThicknessSnapshotEvidence",
+    "RevitWallThicknessSnapshotReadPort",
+]
